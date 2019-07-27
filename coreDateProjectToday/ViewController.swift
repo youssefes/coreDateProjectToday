2467295e13a4b6ae56da78db1a7908de3b0be923
youssef <youssef.esmaileffekygmail.com@youssefs-mac.local>
2019-07-27 15:06:47 +0200

    Initial Commit

---
+++ coreDateProjectToday/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coreDateProjectToday
+//
+//  Created by youssef on 7/27/19.
+//  Copyright © 2019 youssef. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
