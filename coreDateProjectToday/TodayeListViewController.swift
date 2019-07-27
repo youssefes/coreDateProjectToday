283b6a1a9b7e5c9e4c6bbb5ce796c1283ad7eb06
youssef <youssef.esmaileffekygmail.com@youssefs-mac.local>
2019-07-27 15:36:01 +0200

    toDayList add datasource and delegate to tableView

---
+++ coreDateProjectToday/TodayeListViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  coreDateProjectToday
+//
+//  Created by youssef on 7/27/19.
+//  Copyright © 2019 youssef. All rights reserved.
+//
+
+import UIKit
+
+class TodayeListViewController : UITableViewController  {
+
+    let arrayItem = ["Buy item","youssef","mohammed"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrayItem.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodayeListViewController", for: indexPath)
+        cell.textLabel?.text = arrayItem[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+
+}
+
